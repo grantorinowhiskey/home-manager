6cdef127624273fb86f633017cd219438b8978b4
jt <92628121+grantorinowhiskey@users.noreply.github.com>
2025-07-10 21:10:21 +0200

    Added nixvim to modules

--- flake.nix
+++ flake.nix
@@ -26,7 +26,10 @@
 
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
-        modules = [ ./home.nix ];
+        modules = [ 
+        	./home.nix 
+        	nixvim.homeManagerModules.nixvim
+        	];
 
         # Optionally use extraSpecialArgs
         # to pass through arguments to home.nix
